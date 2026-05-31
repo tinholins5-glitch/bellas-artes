e143464ac74f4f5b85de586e6b4fcf3c67e0c8f0
tinholins5-glitch <tinholins5-glitch@users.noreply.github.com>
2026-05-31 12:59:35 -0300

    Adiciona logo ao MeniNicos Kids e consolida organização do workspace

--- push-git.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "%~dp0"
-"C:\Program Files\Git\cmd\git.exe" status --short
-"C:\Program Files\Git\cmd\git.exe" add -A
-"C:\Program Files\Git\cmd\git.exe" commit -m "Deploy do site Bellas's Artes"
-"C:\Program Files\Git\cmd\git.exe" push origin master
-pause
